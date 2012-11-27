7b3397c780ebad9fbf5914f54691b1bca4648388
Elliot Shiu & Ricky Choi <elliot.shiu+rickychoi@gmail.com>
2012-11-26 18:02:10 -0800

    Initial commit, rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiltersIo::Application.config.secret_token = 'e0619f945ce5d4925e6c3f38e0b194a89a5a7a6721ac485f363eb99dd35589c3c047c52ef0448843bc86a7db15e9a6d5902b488ceb63e523ce7e701f231dd295'
