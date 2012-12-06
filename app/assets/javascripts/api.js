78d6a2284052e1bb00cf47df0ee6c15dd95f6365
Phil Moldavski & Ricky Choi <phil.moldavski+rickychoi@gmail.com>
2012-12-05 17:20:41 -0800

    removed deleted files

--- app/assets/javascripts/api.js
+++
@@ -1,9 +0,0 @@
-var API = function(host) {
-  this.prototype.sendPhotoUrl = function(url) {
-    return $.ajax({ type: 'POST'
-    , url: host + '/photos'
-    , dataType: 'json'
-    , data: { photo: {url: url} } 
-    });
-  }
-}
